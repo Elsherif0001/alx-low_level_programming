70d6c696d9e7759d7e0f8d47fcf27ce39380eca3
Elsherif0001 <futuregate0010@gmail.com>
2023-02-20 17:18:39 -0800

    task 4 :Write a program that prints the alphabet in lowercase, followed by a new line. except e and q-only use putchar only use putchar twice

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char sr, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (sr = 'a' ; sr <= 'z' ; sr++)
+	{
+		if (sr != e && sr != q)
+			putchar(sr);
+	}
+	putchar('\n');
+	return (0);
+}
