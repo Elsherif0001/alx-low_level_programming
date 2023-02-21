c946c59d9edc5a560c6250efdbe594b527254e1d
Elsherif0001 <futuregate0010@gmail.com>
2023-02-20 17:24:24 -0800

    task 4 :Write a program that prints the alphabet in lowercase, followed by a new line. except e and q-only use putchar only use putchar twice

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,6 +4,8 @@
 /**
  * main - Entry point
  *
+ *description : Write a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line.
+ *
  * Return Always 0 (Success)
  **/
 int main(void)
