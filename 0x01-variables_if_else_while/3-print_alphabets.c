cd76212c5c6d62c24647a63013454a3bf6f936e0
Elsherif0001 <futuregate0010@gmail.com>
2023-02-20 17:45:26 -0800

    task 5 :Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,7 +4,7 @@
 /**
  * main - Entry point
  *
- *description : a program that prints the alphabet in lowercase, and then in uppercase, in a new line.
+ * Description : a program that prints the alphabet in lowercase, and then in uppercase.
  *
  * Return Always 0 (Success)
  **/
