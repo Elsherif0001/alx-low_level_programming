9199b216949196bb58b156cb9757d0d50c9c7f2e
Elsherif0001 <futuregate0010@gmail.com>
2023-02-20 16:56:19 -0800

    task 2 :Write a program that prints the alphabet in lowercase, followed by a new line. -only use putchar only use putchar twice

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	int o;
+
+	for (o = 'a'; o <= 'z'; o++)
+		putchar(o);
+	putchar('\n');
+	return (0);
+}
